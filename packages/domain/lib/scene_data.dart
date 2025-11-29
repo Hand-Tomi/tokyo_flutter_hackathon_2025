b2925c1d2daf24c589be281cc9a3ecdf0f5bf672
Jiyoun Jeong <jeongjiyoun05@gmail.com>
2025-11-29 23:39:35 +0900

    feat: 전역 변수의 추가

---
+++ packages/domain/lib/scene_data.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'scene_data.freezed.dart';
+part 'scene_data.g.dart';
+
+@freezed
+class SceneData with _$SceneData {
+  const factory SceneData({
+    required String id,
+    String? audioFileName,
+    String? sketchFileName,
+    String? illustrationFileName,
+    String? text,
+  }) = _SceneData;
+
+  factory SceneData.fromJson(Map<String, dynamic> json) =>
+      _$SceneDataFromJson(json);
+}
